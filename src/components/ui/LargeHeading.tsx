2db287c766f97bd1c48282456a70e117b2c2c96a
caolan <caolan@digigrow.ie>
2023-04-23 18:19:54 -0400

    changing file tree structure

--- src/components/ui/LargeHeading.tsx
+++ src/components/ui/LargeHeading.tsx
@@ -5,7 +5,7 @@ import {
   forwardRef,
 } from "react";
 import { VariantProps, cva } from "class-variance-authority";
-import { cn } from "@/app/lib/utils";
+import { cn } from "@/lib/utils";
 
 const largeHeadingVariants = cva(
   "text-black dark:text-white text-center lg:text-left font-extrabold leading-tight tracking-tighter",
